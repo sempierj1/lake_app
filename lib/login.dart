74ac19e6ec11516d05196944baae59cbef70eda7
sempierj1 <36780054+sempierj1@users.noreply.github.com>
2018-03-25 12:02:24 -0400

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -53,11 +53,6 @@ class Choice {
 
 const List<Choice> choices = const <Choice>[
   const Choice(title: 'Login', icon: Icons.account_circle),
-  //const Choice(title: 'BICYCLE', icon: Icons.directions_bike),
-  //const Choice(title: 'BOAT', icon: Icons.directions_boat),
-  //const Choice(title: 'BUS', icon: Icons.directions_bus),
-  //const Choice(title: 'TRAIN', icon: Icons.directions_railway),
-  //const Choice(title: 'WALK', icon: Icons.directions_walk),
 ];
 
 setFirstRun() async
